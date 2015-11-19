c8d834f97587a4ae9c0f4484d80575125559b3b4
Day Davis Waterbury <day.waterbury@gmail.com>
2015-11-19 13:39:52 -0800

    Adding a placeholder excecutable.

--- lib/primetable.rb
+++ lib/primetable.rb
@@ -1,5 +1,7 @@
 require "primetable/version"
 
 module PrimeTable
-  
+  def PrimeTable.run(x)
+    puts "PrimeTable is running..."
+  end
 end
--- spec/primetable_spec.rb
+++ spec/primetable_spec.rb
@@ -4,5 +4,7 @@ describe PrimeTable do
   it 'has a version number' do
     expect(PrimeTable::VERSION).not_to be nil
   end
-  
+  it 'executes when called on the command line' do
+    expect(`primetable`).to eq("PrimeTable is running...\n\n")
+  end  
 end
