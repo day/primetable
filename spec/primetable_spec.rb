2f92745226cb5a47667a55808188280bc71d5ea6
Day Davis Waterbury <day.waterbury@gmail.com>
2015-11-19 17:14:07 -0800

    Updating existing specs and adding a failing spec for passing both -t and -v arguments.

--- spec/primetable_spec.rb
+++ spec/primetable_spec.rb
@@ -7,10 +7,18 @@ describe PrimeTable do
   it 'executes when called on the command line' do
     expect(`primetable`).to eq("PrimeTable is running...\n")
   end
-  it 'prints out a run time when passed a -t argument' do
-    expect(`primetable -t`).to include("ms")
+  it 'executes *and* prints out a run time when passed a -t argument' do
+    execution_output = `primetable -t`
+    expect(execution_output).to include("PrimeTable is running...\n")
+    expect(execution_output).to include("ms")
   end  
   it 'prints out the version number when passed a -v argument' do
   	expect(`primetable -v`).to include(PrimeTable::VERSION)
+  end
+  it 'executes *and* prints out a run time *and* prints out the version number when passed both -t and -v arguments' do
+    execution_output = `primetable -tv`
+    expect(execution_output).to include("PrimeTable is running...\n")
+    expect(execution_output).to include("ms")
+    expect(execution_output).to include(PrimeTable::VERSION)
   end
 end
