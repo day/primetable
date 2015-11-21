6ba4f38384b458f9906dee18f8139250ff11d50b
Day Davis Waterbury <day.waterbury@gmail.com>
2015-11-20 20:54:43 -0800

    Removing a bit of debug output I inadvertently left in a couple commits back.

--- lib/primetable.rb
+++ lib/primetable.rb
@@ -1,6 +1,5 @@
 # Require modules from the lib/primetable directory...right now it's just our VERSION constant
 Dir["#{File.dirname(__FILE__)}/primetable/**/*.rb"].each { |f| require(f) }
-puts PrimeTable::VERSION
 
 # This is needed for load_primes method, which loads data from a file.
 require "yaml"
